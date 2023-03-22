fb35ec3d6e750f7a8bf1cc26a85f5c853818aa42
Slava Rybkin <yolka.sd@gmail.com>
2023-03-22 22:43:21 +0700

    Task - 2

--- src/main/java/org/example/Book.java
+++ src/main/java/org/example/Book.java
@@ -1,2 +1,40 @@
-package org.example;public class Book {
+package org.example;
+
+public class Book {
+    private String title;
+    private String author;
+    private String published;
+
+    public Book() {
+    }
+
+    public Book(String title, String author, String published) {
+        this.title = title;
+        this.author = author;
+        this.published = published;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getPublished() {
+        return published;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setPublished(String published) {
+        this.published = published;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,9 @@
 package org.example;
+
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JavaType;
 import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -13,13 +15,18 @@ public class Main {
         //Task - 1
         File personJSON = new File("src/main/resources/person.json");
         List<Person> personList = readJsonFile(personJSON, Person.class);
-        System.out.println(personList);
+        //Task - 2
+        File bookJSON = new File("src/main/resources/book.json");
+        List<Book> bookList = new ArrayList<>();
+        readJsonFile2(bookJSON, Book.class, bookList);
+
+
 
     }
 
     public static <T> List<T> readJsonFile(File json, Class<T> clazz) {
         List<T> list = new ArrayList<>();
-        try (FileInputStream inputStream = new FileInputStream(json)){
+        try (FileInputStream inputStream = new FileInputStream(json)) {
             ObjectMapper mapper = new ObjectMapper();
             JavaType type = mapper.getTypeFactory().constructCollectionType(List.class, clazz);
             list = mapper.readValue(inputStream, type);
@@ -28,5 +35,16 @@ public class Main {
             System.err.println("Error! Failed to read file: " + json.getAbsolutePath() + ". " + e.getMessage());
         }
         return list;
+    }
+
+    public static <T> void readJsonFile2(File json, Class<T> clazz, List<T> list) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            JavaType type = mapper.getTypeFactory().constructCollectionType(List.class, clazz);
+            List<T> newList = mapper.readValue(json, type);
+            list.addAll(newList);
+        } catch (IOException e) {
+            System.err.println("Error! Failed to read file: " + json.getAbsolutePath() + ". " + e.getMessage());
+        }
     }
 }
